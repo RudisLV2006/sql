468c1899e259f4c769978c8d604cea76d9a41a42
Kristaps <your@email.com>
2024-09-06 10:35:42 +0300

    Select with like

---
+++ select/5.sql
@@ -0,0 +1,30 @@
+SELECT 
+    *
+FROM
+    customers
+WHERE
+    last_name LIKE 'b%';
+    
+    
+SELECT 
+    *
+FROM
+    customers
+WHERE
+    last_name LIKE 'b_________%';
+    
+    
+    
+SELECT 
+    *
+FROM
+    customers
+WHERE
+    last_name LIKE 'b_t%';
+    
+SELECT 
+    *
+FROM
+    customers
+WHERE
+    last_name LIKE '%e%';
